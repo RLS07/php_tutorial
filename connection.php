bf731deb845f41eed4be59737441f06e4483b9f8
RLS07 <lalpower77@gmail.com>
2025-01-22 08:02:16 +0545

    create conn db
    .

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$host="localhost";
+$dbuser="root";
+$dbpass="";
+$dbname="iimscms";
+//making connetion
+$conn=mysqli_connect($host, $dbuser, $dbpass, $dbname) or die("Unable to connect to database");
+?>
--- templates/sidebar.php
+++ templates/sidebar.php
@@ -8,7 +8,7 @@
           <img src="assets/img/user2-160x160.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>Subin Shakya</p>
+          <p>Rojan Shrestha</p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
